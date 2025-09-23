65cbe7c7ad5e239a5f342d3741b58058aad50065
Jorge Paz <jorgefpaz011@gmail.com>
2025-09-23 16:35:54 -0600

    Added main page and button component

---
+++ heladeriaUI/src/pages/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Button } from '../components/button.jsx'
+
+const main = () => {
+  return (
+    <div>
+        <h1>Main Page</h1>
+        <Button />
+    </div>
+  )
+}
+
+export default main
