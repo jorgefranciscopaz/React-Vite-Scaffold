7880e49dd518a1cd1d5e177de534e6646f20b721
Jorge Paz <jorgefpaz011@gmail.com>
2025-09-23 16:49:45 -0600

    created styles sheet for button and main

--- heladeriaUI/src/components/button.jsx
+++ heladeriaUI/src/components/button.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
-export const Button = () => {
+// Definición del botón
+const Button = ({ title = 'Button', color = 'blue', onClick }) => {
   return (
-    <button>Button</button>
+    <button 
+      style={{ backgroundColor: color, color: '#fff', padding: '8px 16px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}
+      onClick={onClick}
+    >
+      {title}
+    </button>
   )
 }
+
+export default Button
--- heladeriaUI/src/pages/main.jsx
+++ heladeriaUI/src/pages/main.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
+import './main.css'
+import Button from '../components/button.jsx'
 
-import { Button } from '../components/button.jsx'
-
-const main = () => {
+const Main = () => {
   return (
     <div>
-        <h1>Main Page</h1>
-        <Button />
+      <h1>Main Page</h1>
+      <Button 
+        title="Click me" 
+        color="blue" 
+        onClick={() => alert('Button clicked!')} 
+      />
+      <Button 
+        title="Dont Click me" 
+        color="red" 
+        onClick={() => alert('Button clicked!')} 
+      />
+      <Button 
+        title="ninja" 
+        color="black" 
+        onClick={() => alert('NINJA')} 
+      />
+      <Button 
+        title="Click me" 
+        color="green" 
+        onClick={() => alert('Terminating process')} 
+      />
     </div>
   )
 }
 
-export default main
+export default Main
